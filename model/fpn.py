0fd84c0940a9c0e8ac2582d36fc8f97923d838ff
haraldger <harald.gerhardsen@hotmail.com>
2023-05-03 20:18:17 -0400

    Fix comment

--- model/fpn.py
+++ model/fpn.py
@@ -46,7 +46,7 @@ class FPN(nn.Module):
         out.append(self.relu(self.p6_conv(out[-1])))
         out.append(self.relu(self.p7_conv(out[-1])))
 
-        return out[::-1]    # [P2, P3, P4, P5, P6, P7]
+        return out[::-1]    # [P3, P4, P5, P6, P7]
 
 
 
