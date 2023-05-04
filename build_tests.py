d8934b557e6feb08ba2027597d278d128e28a716
Koushik-S <koushiksrinivasula07@gmail.com>
2023-05-03 21:50:11 -0400

    Implement protonet|| create unit tests

--- build_tests.py
+++ build_tests.py
@@ -1,7 +1,8 @@
-from model import resnet_backbone
+from model import resnet_backbone,protonet
 
 def main():
     resnet_backbone.run_tests()
+    protonet.run_tests()
 
 if __name__ == '__main__':
     main()
