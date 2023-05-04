76d223dff47a1709b0de1bb51bc7f37d4ebcd766
haraldger <harald.gerhardsen@hotmail.com>
2023-05-04 15:40:44 -0400

    Implement prediction heads module. Create unit tests for pred heads.

--- build_tests.py
+++ build_tests.py
@@ -1,8 +1,9 @@
-from model import resnet_backbone, fpn
+from model import resnet_backbone, fpn, prediction_heads
 
 def main():
     resnet_backbone.run_tests()
     fpn.run_tests()
+    prediction_heads.run_tests()
 
 if __name__ == '__main__':
     main()
