fa68ce2ff38c5dfbf3af1938b33a2844f5309c9d
haraldger <harald.gerhardsen@hotmail.com>
2023-05-05 14:51:20 -0400

    Implement first stage of Yolact - missing fast NMS and mask assembly.

--- build_tests.py
+++ build_tests.py
@@ -1,10 +1,11 @@
-from model import resnet_backbone, fpn, prediction_heads, protonet
+from model import resnet_backbone, fpn, prediction_heads, protonet, yolact
 
 def main():
     resnet_backbone.run_tests()
     fpn.run_tests()
     prediction_heads.run_tests()
     protonet.run_tests()
+    yolact.run_tests()
 
 
 if __name__ == '__main__':
--- model/yolact.py
+++ model/yolact.py
@@ -5,34 +5,55 @@ import torch.nn.functional as F
 from . import prediction_heads, resnet_backbone, fpn, protonet
 
 class Yolact(nn.Module):
-    def __init__(self, backbone=None):
+    def __init__(self, backbone=None, num_classes=2, num_masks=32):
         super().__init__()
 
+        self.num_classes = num_classes
+        self.num_masks = num_masks
+
+        # Network components
         if backbone is None:
             self.backbone = resnet_backbone.resnet50()
         else:
             self.backbone = backbone
-
         self.fpn = fpn.FPN()
-        self.prediction_heads = prediction_heads.PredictionHeads()
-        #self.protonet = protonet.Protonet()
-
-        self.bbox_format = torch.tensor([1, 1, 1, 1, 1, 1, 1, 1], dtype=torch.float32)
+        self.prediction_heads = prediction_heads.PredictionHeads(num_classes=self.num_classes, num_masks=self.num_masks)
+        self.protonet = protonet.Protonet()
 
     def forward(self, x):
-        raise NotImplementedError
+        # Backbone + FPN
+        c_outputs = self.backbone(x)
+        p_outputs = self.fpn(c_outputs)
 
-    def postprocess(self, cls_logits, reg_logits, mask_logits, anchors, h, w, top_k=100, score_threshold=0.05, nms_threshold=0.5):
-        # cls_logits: list of num_heads tensors of shape [N, num_anchors*num_classes, H, W]
-        # reg_logits: list of num_heads tensors of shape [N, num_anchors*4, H, W]
-        # mask_logits: list of num_heads tensors of shape [N, num_anchors*num_masks, H, W]
-        # anchors: list of num_heads tensors of shape [num_anchors, 4]
-        # h: list of num_heads tensors of shape [N]
-        # w: list of num_heads tensors of shape [N]
-        # top_k: int
-        # score_threshold: float
-        # nms_threshold: float
+        # Prediction heads
+        cls_preds, reg_preds, mask_coefficients = self.prediction_heads(p_outputs)
+        for idx in range(len(cls_preds)):
+            cls_preds[idx] = cls_preds[idx].permute(0, 2, 3, 1).contiguous().view(x.shape[0], -1, self.num_classes)
+            reg_preds[idx] = reg_preds[idx].permute(0, 2, 3, 1).contiguous().view(x.shape[0], -1, 4)
+            mask_coefficients[idx] = mask_coefficients[idx].permute(0, 2, 3, 1).contiguous().view(x.shape[0], -1, self.num_masks)
+        
+        cls = torch.cat(cls_preds, dim=1)
+        reg = torch.cat(reg_preds, dim=1)
+        mask_coefficients = torch.cat(mask_coefficients, dim=1)
 
-        # Output shapes:
-        # bboxes
-        raise NotImplementedError
+        # TODO: Perform Fast NMS
+
+        # ProtoNet
+        proto_out = self.protonet(p_outputs[0])
+
+        return torch.randn(2, 256, 69, 69)
+        
+
+
+    
+# testing
+
+def test_yolact_forward():
+    x = torch.randn(2, 3, 550, 550)
+    model = Yolact()
+    y = model(x)
+
+    # TODO: Implement meaningful test with assertions
+
+def run_tests():
+    test_yolact_forward()
